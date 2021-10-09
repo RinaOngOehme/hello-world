460cf183f695cdfc666c940f42af25a274c09016
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-10-09 14:36:42 +0400

    Update Start.js
    updated navigation, name and selected backcolor

--- components/Start.js
+++ components/Start.js
@@ -1,36 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button, ImageBackground, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, ImageBackground, TouchableOpacity, Image } from 'react-native';
 
 
 export default class Start extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { username: '', };
+    this.state = { name: '', text: '', backColor: '' };
   }
 
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.box}>
 
         {/*include background image */}
         <ImageBackground source={require("../assets/BackgroundImage.png")} resizeMode="cover" style={styles.image}>
           <Text style={styles.appTitle}>Hello World Chat</Text>
-          <View style={styles.textInputContainer}>
+
+          <View style={styles.inputNameColorContainer}>
 
             {/*container for icon, name textinput,   */}
-            <View >
-              <Image source={require("../assets/icon.svg")} />
-              <TextInput
-                style={styles.textInputStyle}
-                onChangeText={(username) => this.setState({ username })}
-                value={this.state.username}
+            <View style={styles.textInputContainer} >
+              <Image source={require("../assets/icon.svg")} style={styles.inputImageStyle} />
+              <TextInput style={styles.textInputStyle}
+                onChangeText={(name) => this.setState({ name })}
+                value={this.state.name}
                 placeholder='Your Name'
               />
-              <Text>You wrote: {this.state.text}</Text>
             </View>
 
             {/*color selector for user background */}
-            <View >
+            <View style={styles.backgroundContainer}>
               <Text style={styles.backgroundColorText}> Choose Background Color: </Text>
 
               <View style={styles.selectBgColor}>
@@ -51,17 +50,18 @@ export default class Start extends React.Component {
                   onPress={() => this.setState({ backColor: '#B9C6AE' })}
                 />
               </View>
+
             </View>
 
             {/*button to go into chat room */}
             <View style={styles.buttonContainer}>
-              <Button style={styles.startChatStyle}
-                onPress={() => {
-                  this.alertMyText({ text: this.state.text });
-                }}
+              <Button color='#757083' style={styles.startChatStyle}
                 title="Start Chatting"
+                onPress={() => this.props.navigation.navigate('Chat',
+                  { name: this.state.name, backColor: this.state.backColor, })}
               />
             </View>
+
           </View>
         </ImageBackground>
       </View>
@@ -70,55 +70,87 @@ export default class Start extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  box: {
     flex: 1,
-    flexDirection: 'column'
+
   },
+
   image: {
     flex: 1,
     justifyContent: 'center'
   },
+
   appTitle: {
     color: '#FFFFFF',
     fontSize: 45,
     fontWeight: '600',
     textAlign: 'center',
     justifyContent: 'space-evenly',
-    flex: 0.8,
-    backgroundColor: '#000000c0'
+    flex: 0.7
   },
+
+  inputNameColorContainer: {
+    backgroundColor: 'white',
+    flex: 0.44,
+    height: 30,
+    margin: 12
+  },
+
   textInputContainer: {
-    flex: 50,
-    color: 'white',
-    borderColor: '#8A95A5',
-    lineHeight: 84,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: '#000000c0'
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor: '#fff',
+    borderWidth: 0.5,
+    borderColor: '#000',
+    height: 40,
+    borderRadius: 5,
+    margin: 10,
+
   },
+
+  inputImageStyle: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode: 'stretch',
+    alignItems: 'center',
+  },
+
   textInputStyle: {
     color: '#757083',
     fontSize: 16,
     fontWeight: '200',
-
+    opacity: 0.5,
     textAlign: 'left',
-    backgroundColor: '#000000c0'
+    height: 30,
+    margin: 5,
+
   },
+
+  backgroundContainer: {
+    margin: 5,
+
+  },
+
 
   backgroundColorText: {
     color: '#757083',
     fontSize: 16,
-
+    opacity: 1,
     fontWeight: '300',
-    textAlign: 'center',
-    backgroundColor: '#000000c0'
+    textAlign: 'left',
+    margin: 12,
+
   },
   selectBgColor: {
-    backgroundColor: '#000000c0',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     width: 50,
     height: 50,
-    borderRadius: 25,
     margin: 5,
+
   },
 
   colorSelect1: {
@@ -128,6 +160,7 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     margin: 5,
   },
+
   colorSelect2: {
     backgroundColor: '#474056',
     width: 50,
@@ -135,12 +168,14 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     margin: 5,
   },
+
   colorSelect3: {
     backgroundColor: '#8A95A5',
     width: 50,
     height: 50,
     borderRadius: 25,
     margin: 5,
+
   },
   colorSelect4: {
     backgroundColor: '#B9C6AE',
@@ -149,15 +184,19 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     margin: 5,
   },
+
   buttonContainer: {
-    backgroundColor: '#757083'
+    margin: 12,
+
   },
+
   startChatStyle: {
     color: '#FFFFFF',
     fontSize: 16,
     fontWeight: '600',
     textAlign: 'center',
-    justifyContent: 'space-evenly',
+
+
   },
 
 });
